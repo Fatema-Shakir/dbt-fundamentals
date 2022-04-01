a5131c877de86deac1b2c8e3b0e96ad1f4ec7d34
fatemashakir42@gmail.com <fatemashakir42@gmail.com>
2022-04-01 07:12:05 +0000

    commit3

---
+++ macros/fees.sql
@@ -0,0 +1,8 @@
+{% macro fees_installment(Fees) %}
+    ({{ Fees }})
+{% endmacro %}
+
+
+/*{% macro cents_to_dollars(column_name, precision=2) %}
+    ({{ column_name }} / 100)::numeric(16, {{ precision }})
+{% endmacro %}*/
